28d237a14165e74c32c8bfec8c186c296b3184a1
Alejandro Reyes <reyesbalejandroe@gmail.com>
2023-03-06 01:20:02 +0100

    update/pagination

--- controllers/productsApi.js
+++ controllers/productsApi.js
@@ -5,13 +5,15 @@ const Product = require("../models/productsApi"); // BBDD
 // GET http://localhost:3000/api/products
 
 const getProduct = async (req, res) => {
-    const page = parseInt(req.query.page);
-    const limit = parseInt(req.query.limit);
-    const skipIndex = (page - 1) * limit;
-    let products = [];
 
-    try {//Pagination
-        if (page!=NaN && limit!=NaN) {
+    let products = [];
+    const pagination = req.query.hasOwnProperty('page') && req.query.hasOwnProperty('limit');
+    try {
+        if (pagination) {//Pagination
+            const page = parseInt(req.query.page);
+            const limit = parseInt(req.query.limit);
+            const skipIndex = (page - 1) * limit;
+
             products = await Product.find({}, ' -_id -__v')
                 .sort({ _id: 1 })
                 .limit(limit)
@@ -21,12 +23,13 @@ const getProduct = async (req, res) => {
             res.status(200).json(products); //Devuelve el producto
         } else {//Get all products
             products = await Product.find({}, ' -_id -__v');
-            res.status(200).json(allProducts); // Devuelve todos los datos
+            res.status(200).json(products); // Devuelve todos los datos
         }
     } catch (err) {
         res.status(400).json({ message: err });
     }
 }
+
 
 const createProduct = async (req, res) => {
     console.log(req.body); // Objeto recibido de producto nuevo
